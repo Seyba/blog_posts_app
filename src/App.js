f168c69b5fb01b5c691727337a647927e4f7f6b9
Mahamadou <mmohmo@yahoo.com>
2023-08-11 23:54:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PostPage from './pages/PostPage';
+import EditPostPage from './pages/EditPostPage';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path='/' element={<HomePage />} />
+        <Route path='/posts/:id' element={<PostPage />}/>
+        <Route path='/posts/edit/:id' element={<EditPostPage />}/>
+
+        <Route path='*' element={<NotFound />}/>
+
+      </Routes>
     </div>
   );
 }
